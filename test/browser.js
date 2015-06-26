769f12bcc5cf7d3cbb403f267040e2107d7ccc87
Dani723 <dani@dk2.de>
2015-06-26 10:27:34 +0200

    refactor iri parser

--- test/browser.js
+++ test/browser.js
@@ -65,11 +65,16 @@ describe("helpers", function() {
   it("should have static helpers", function() {
     assert.strictEqual(typeof Pajax.qsParse, 'function');
     assert.strictEqual(typeof Pajax.qsStringify, 'function');
-    assert.strictEqual(typeof Pajax.isIRI, 'function');
-    assert.strictEqual(typeof Pajax.parseIRI, 'function');
+    assert.strictEqual(typeof Pajax.isURL, 'function');
+    assert.strictEqual(typeof Pajax.parseURL, 'function');
     assert.strictEqual(typeof Pajax.clone, 'function');
     assert.strictEqual(typeof Pajax.defaults, 'function');
     assert.strictEqual(typeof Pajax.merge, 'function');
+
+    var parsedURL = Pajax.parseURL('http://www.foo.net/bar');
+
+    assert.strictEqual(parsedURL.isRelative, false);
+    assert.strictEqual(parsedURL.host, 'www.foo.net');
   });
 });
 

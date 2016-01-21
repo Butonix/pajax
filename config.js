018f46cc7c3682ae45c0fce321529e3aecd5ce29
Dani723 <dani@dk2.de>
2016-01-21 14:04:51 +0100

    Upgrade deps

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ System.config({
   },
 
   map: {
-    "babel": "npm:babel-core@5.8.34",
-    "babel-runtime": "npm:babel-runtime@5.8.34",
+    "babel": "npm:babel-core@5.8.35",
+    "babel-runtime": "npm:babel-runtime@5.8.35",
     "chai": "npm:chai@3.4.1",
     "core-js": "npm:core-js@1.2.6",
     "jspm-testem": "github:OrKoN/jspm-testem@master",
@@ -43,7 +43,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.8.34": {
+    "npm:babel-runtime@5.8.35": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:buffer@3.5.2": {
